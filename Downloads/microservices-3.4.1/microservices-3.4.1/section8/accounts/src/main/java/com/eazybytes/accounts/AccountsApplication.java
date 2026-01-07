fdb92275b55d9444a2a76cb386bf166cb966ebd4
sankeerthsiram <sankeerth.siram@gmail.com>
2026-01-07 08:56:16 +0530

    commit in feature for comments

--- Downloads/microservices-3.4.1/microservices-3.4.1/section8/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ Downloads/microservices-3.4.1/microservices-3.4.1/section8/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -12,6 +12,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//adding these comments for commit
+
 @SpringBootApplication
 @EnableFeignClients
 /*@ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller") })
